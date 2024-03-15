275fe6c870af5250847712742358c5b42e7c69cf
02bbits <tuannguyen4112k5@gmail.com>
2024-03-15 22:29:29 +0700

    add the following functions: rotate right, sigma 0, sigma 1

--- main.cpp
+++ main.cpp
@@ -5,9 +5,23 @@
 
 using namespace std;
 
+std::bitset<8> rotr(char x, int n) {
+	return (std::bitset<8>(x) >> n) | (std::bitset<8>(x) << (32 - n));
+}
+
+std::bitset<8> sig0(char x) {
+	return rotr(x, 7) ^ rotr(x, 18) ^ (std::bitset<8>(x) >> 3);
+}
+
+std::bitset<8> sig1(char x) {
+    return rotr(x, 17) ^ rotr(x, 19) ^ (std::bitset<8>(x) >> 10);
+}
 
 int main() {
-    string str = "hello, world!";
-    cout << stringToBinary(str) << endl;
-    cout << pad(stringToBinary(str));
+    char x = 'f';
+    cout << std::bitset<8>(x) << endl;
+    cout << sig0('c') << endl;
+    cout << sig1('c') << endl;
+    return 0;
+
 }
